fac961ee3665218b45189f2fb68879c302408381
louis <louis@systemli.org>
2018-05-17 15:54:00 +0200

    replace links, hashtags and mentions in message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -10,7 +10,7 @@ export default class Message extends React.Component {
 
         this.state = {
             id: props.message.id,
-            text: props.message.text,
+            text: Message._replaceMagic(props.message.text),
             creationDate: props.message.creation_date
         };
 
@@ -18,8 +18,16 @@ export default class Message extends React.Component {
         this._deleteMessage = this._deleteMessage.bind(this);
     }
 
+    static _replaceMagic(text) {
+        return (text
+            .replace(/(https?:\/\/([^\s]+))/g, '<a href="$1" target="_blank">$2</a>')
+            .replace(/#(\S+)/g, '<a target="_blank" href="https://twitter.com/search?q=%23$1">#$1</a>')
+            .replace(/@(\S+)/g, '<a target="_blank" href="https://twitter.com/$1">@$1</a>')
+            .replace(/(?:\r\n|\r|\n)/g, '<br/>'));
+    }
+
     _getText() {
-        return (<p>{this.state.text}</p>);
+        return (<p dangerouslySetInnerHTML={{ __html: this.state.text }} />);
     }
 
     _deleteMessage(event) {
