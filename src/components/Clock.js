6570697c06f6c5bcca03ce689ecc66d32dad28d7
louis <louis@systemli.org>
2018-05-17 19:18:37 +0200

    add clock component

---
+++ src/components/Clock.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Moment from "react-moment";
+
+export default class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            format: props.format || null,
+        };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <p>
+                <Moment format={this.state.format}>{this.state.date}</Moment>
+            </p>
+        );
+    }
+}
