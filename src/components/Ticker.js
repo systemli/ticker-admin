83213cad29b9763333e810cabd748bbc0a6008dd
louis <louis@systemli.org>
2018-05-19 19:01:46 +0200

    make delete/use buttons optional

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -27,20 +27,14 @@ export default class Ticker extends React.Component {
             },
             confirmOpen: false,
             modalOpen: false,
+            useButton: props.use || false,
+            deleteButton: props.delete || false,
         };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleConfirm = this.handleConfirm.bind(this);
-    }
-
-    handleInputChange(event, data) {
-        const value = data.type === 'checkbox' ? data.checked : data.value;
-        const name = data.name;
-
-        if (name !== undefined && value !== undefined) {
-            this.form[name] = value;
-        }
+        this.renderUseButton = this.renderUseButton.bind(this);
+        this.renderDeleteButton = this.renderDeleteButton.bind(this);
     }
 
     handleSubmit(event) {
@@ -74,6 +68,96 @@ export default class Ticker extends React.Component {
 
     getForm() {
         return (
+            <Form onSubmit={this.handleSubmit} id='editTicker'>
+                <Form.Group widths='equal'>
+                    <Form.Input
+                        label='Title'
+                        name='title'
+                        defaultValue={this.state.ticker.title}
+                        onChange={(event, input) => this.form.title = input.value}
+                    />
+                    <Form.Input
+                        label='Domain'
+                        name='domain'
+                        defaultValue={this.state.ticker.domain}
+                        onChange={(event, input) => this.form.domain = input.value}
+                    />
+                </Form.Group>
+                <Form.Checkbox
+                    toggle
+                    label='Active'
+                    name='active'
+                    defaultChecked={this.state.ticker.active}
+                    onChange={(event, input) => this.form.active = input.checked}
+                />
+                <Form.TextArea
+                    label='Description'
+                    name='description'
+                    rows='5'
+                    defaultValue={this.state.ticker.description}
+                    onChange={(event, input) => this.form.description = input.value}
+                />
+                <Header dividing>Information</Header>
+                <Form.Group widths='equal'>
+                    <Form.Input label='Author'>
+                        <Input
+                            iconPosition='left'
+                            placeholder='Author' name='information.author'
+                            defaultValue={this.state.ticker.information.author}
+                            onChange={(event, input) => this.form.information.author = input.value}>
+                            <Icon name='users'/>
+                            <input/>
+                        </Input>
+                    </Form.Input>
+                    <Form.Input label='Homepage'>
+                        <Input
+                            iconPosition='left'
+                            name='information.url'
+                            defaultValue={this.state.ticker.information.url}
+                            onChange={(event, input) => this.form.information.url = input.value}>
+                            <Icon name='home'/>
+                            <input/>
+                        </Input>
+                    </Form.Input>
+                </Form.Group>
+                <Form.Group widths='equal'>
+                    <Form.Input label='Email'>
+                        <Input
+                            iconPosition='left'
+                            placeholder='Email' name='information.email'
+                            defaultValue={this.state.ticker.information.email}
+                            onChange={(event, input) => this.form.information.email = input.value}>
+                            <Icon name='at'/>
+                            <input/>
+                        </Input>
+                    </Form.Input>
+                    <Form.Input label='Twitter'>
+                        <Input
+                            iconPosition='left'
+                            name='information.twitter'
+                            defaultValue={this.state.ticker.information.twitter}
+                            onChange={(event, input) => this.form.information.twitter = input.value}>
+                            <Icon name='twitter'/>
+                            <input/>
+                        </Input>
+                    </Form.Input>
+                    <Form.Input label='Facebook'>
+                        <Input
+                            iconPosition='left'
+                            name='information.facebook'
+                            defaultValue={this.state.ticker.information.facebook}
+                            onChange={(event, input) => this.form.information.facebook = input.value}>
+                            <Icon name='facebook'/>
+                            <input/>
+                        </Input>
+                    </Form.Input>
+                </Form.Group>
+            </Form>
+        );
+    }
+
+    getModal() {
+        return (
             <Modal trigger={<Button color='black' icon='edit' content='edit'
                                     onClick={() => this.setState({modalOpen: true})}/>}
                    dimmer='blurring' closeOnRootNodeClick={false} open={this.state.modalOpen} closeIcon
@@ -82,91 +166,7 @@ export default class Ticker extends React.Component {
                    style={{marginTop: '0px !important', marginLeft: 'auto', marginRight: 'auto'}}>
                 <Header>Edit {this.state.ticker.title}</Header>
                 <Modal.Content>
-                    <Form onSubmit={this.handleSubmit} id='editTicker'>
-                        <Form.Group widths='equal'>
-                            <Form.Input
-                                label='Title'
-                                name='title'
-                                defaultValue={this.state.ticker.title}
-                                onChange={this.handleInputChange}
-                            />
-                            <Form.Input
-                                label='Domain'
-                                name='domain'
-                                defaultValue={this.state.ticker.domain}
-                                onChange={this.handleInputChange}
-                            />
-                        </Form.Group>
-                        <Form.Checkbox
-                            toggle
-                            label='Active'
-                            name='active'
-                            defaultChecked={this.state.ticker.active}
-                            onChange={this.handleInputChange}
-                        />
-                        <Form.TextArea
-                            label='Description'
-                            name='description'
-                            rows='5'
-                            defaultValue={this.state.ticker.description}
-                            onChange={this.handleInputChange}
-                        />
-                        <Header dividing>Information</Header>
-                        <Form.Group widths='equal'>
-                            <Form.Input label='Author'>
-                                <Input
-                                    iconPosition='left'
-                                    placeholder='Author' name='information.author'
-                                    defaultValue={this.state.ticker.information.author}
-                                    onChange={(event, input) => this.form.information.author = input.value}>
-                                    <Icon name='users'/>
-                                    <input/>
-                                </Input>
-                            </Form.Input>
-                            <Form.Input label='Homepage'>
-                                <Input
-                                    iconPosition='left'
-                                    name='information.url'
-                                    defaultValue={this.state.ticker.information.url}
-                                    onChange={(event, input) => this.form.information.url = input.value}>
-                                    <Icon name='home'/>
-                                    <input/>
-                                </Input>
-                            </Form.Input>
-                        </Form.Group>
-                        <Form.Group widths='equal'>
-                            <Form.Input label='Email'>
-                                <Input
-                                    iconPosition='left'
-                                    placeholder='Email' name='information.email'
-                                    defaultValue={this.state.ticker.information.email}
-                                    onChange={(event, input) => this.form.information.email = input.value}>
-                                    <Icon name='at'/>
-                                    <input/>
-                                </Input>
-                            </Form.Input>
-                            <Form.Input label='Twitter'>
-                                <Input
-                                    iconPosition='left'
-                                    name='information.twitter'
-                                    defaultValue={this.state.ticker.information.twitter}
-                                    onChange={(event, input) => this.form.information.twitter = input.value}>
-                                    <Icon name='twitter'/>
-                                    <input/>
-                                </Input>
-                            </Form.Input>
-                            <Form.Input label='Facebook'>
-                                <Input
-                                    iconPosition='left'
-                                    name='information.facebook'
-                                    defaultValue={this.state.ticker.information.facebook}
-                                    onChange={(event, input) => this.form.information.facebook = input.value}>
-                                    <Icon name='facebook'/>
-                                    <input/>
-                                </Input>
-                            </Form.Input>
-                        </Form.Group>
-                    </Form>
+                    {this.getForm()}
                 </Modal.Content>
                 <Modal.Actions>
                     <Button.Group>
@@ -177,6 +177,22 @@ export default class Ticker extends React.Component {
                 </Modal.Actions>
             </Modal>
         );
+    }
+
+    renderUseButton() {
+        if (this.state.useButton) {
+            return (
+                <Button color='teal' icon='rocket' content='Use' href={`/ticker/${this.state.ticker.id}`}/>
+            );
+        }
+    }
+
+    renderDeleteButton() {
+        if (this.state.deleteButton) {
+            return (
+                <Button color='red' icon='delete' content='Delete' onClick={this.openDeleteModal}/>
+            );
+        }
     }
 
     render() {
@@ -195,20 +211,20 @@ export default class Ticker extends React.Component {
                 </Card.Content>
                 <Card.Content>
                     <Button.Group size='tiny' fluid compact>
-                        <Button color='teal' icon='rocket' content='Use' href={`/ticker/${this.state.ticker.id}`}/>
-                        {this.getForm()}
-                        <Button color='red' icon='delete' content='Delete' onClick={this.openDeleteModal}/>
-                        <Confirm
-                            open={this.state.confirmOpen}
-                            onCancel={this.handleCancel}
-                            onConfirm={this.handleConfirm}
-                            dimmer='blurring'
-                            size='mini'
-                            //https://github.com/Semantic-Org/Semantic-UI-React/issues/2558
-                            style={{marginTop: '0px !important', marginLeft: 'auto', marginRight: 'auto'}}
-                        />
+                        {this.renderUseButton()}
+                        {this.getModal()}
+                        {this.renderDeleteButton()}
+
                     </Button.Group>
                 </Card.Content>
+                <Confirm open={this.state.confirmOpen}
+                         onCancel={this.handleCancel}
+                         onConfirm={this.handleConfirm}
+                         dimmer='blurring'
+                         size='mini'
+                    //https://github.com/Semantic-Org/Semantic-UI-React/issues/2558
+                         style={{marginTop: '0px !important', marginLeft: 'auto', marginRight: 'auto'}}
+                />
             </Card>
         );
     }
