c52ba370d41b4fe9a11dd9ccae80b8dde81f4199
louis <louis@systemli.org>
2018-05-20 01:56:56 +0200

    add logout button to navigation

--- src/views/Navigation.js
+++ src/views/Navigation.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import {Container, Image, Menu} from 'semantic-ui-react';
+import {Container, Icon, Image, Menu} from 'semantic-ui-react';
 import Clock from "../components/Clock";
+import AuthService from "../components/AuthService";
+import {withRouter} from "react-router-dom";
 
-export default class Navigation extends React.Component {
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.Auth = new AuthService();
+    }
+
+    renderUserItem() {
+        if (this.Auth.loggedIn()) {
+            return (
+                <Menu.Item onClick={() => {
+                    this.Auth.logout();
+                    this.props.history.replace("/login");
+                }}><Icon name='user'/>Logout</Menu.Item>
+            );
+        }
+    }
+
     render() {
         return (
             <Menu fixed='top' size='tiny' inverted>
                 <Container>
-                    <Menu.Item><Image src='/logo.png' spaced='right' style={{position: 'absolute', right: 0}}/></Menu.Item>
+                    <Menu.Item><Image src='/logo.png' spaced='right'
+                                      style={{position: 'absolute', right: 0}}/></Menu.Item>
                     <Menu.Item href='/' active={window.location.pathname === '/'}><strong>Home</strong></Menu.Item>
-                    <Menu.Item position='right'><Clock format='dddd, YY/MM/DD, HH:mm:ss'/></Menu.Item>
+                    <Menu.Menu position='right'>
+                        <Menu.Item><Clock format='dddd, YY/MM/DD, HH:mm:ss'/></Menu.Item>
+                        {this.renderUserItem()}
+                    </Menu.Menu>
                 </Container>
             </Menu>
         );
     }
 }
+
+export default withRouter(Navigation);
