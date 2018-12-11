a30541ea220dab5cbc76dc8daebd4511cc844ae2
louis <louis@systemli.org>
2018-12-11 19:35:34 +0100

    check if information and twitter is in props before assign to model

--- src/models/Ticker.js
+++ src/models/Ticker.js
@@ -14,18 +14,24 @@ export default class Ticker {
         this.description = undefined !== props.description ? props.description : null;
         this.active = undefined !== props.active ? props.active : false;
         this.prepend_time = undefined !== props.prepend_time ? props.prepend_time : false;
-        this.information = {
-            author: undefined !== props.information.author ? props.information.author : null,
-            url: undefined !== props.information.url ? props.information.url : null,
-            email: undefined !== props.information.email ? props.information.email : null,
-            twitter: undefined !== props.information.twitter ? props.information.twitter : null,
-            facebook: undefined !== props.information.facebook ? props.information.facebook : null,
-        };
-        this.twitter = {
-            active: undefined !== props.twitter.active ? props.twitter.name : false,
-            name: undefined !== props.twitter.name ? props.twitter.name : null,
-            screen_name: undefined !== props.twitter.screen_name ? props.twitter.screen_name : null,
-            description: undefined !== props.twitter.description ? props.twitter.description : null,
+
+        if (undefined !== props.information) {
+            this.information = {
+                author: undefined !== props.information.author ? props.information.author : null,
+                url: undefined !== props.information.url ? props.information.url : null,
+                email: undefined !== props.information.email ? props.information.email : null,
+                twitter: undefined !== props.information.twitter ? props.information.twitter : null,
+                facebook: undefined !== props.information.facebook ? props.information.facebook : null,
+            };
+        }
+
+        if (undefined !== props.twitter) {
+            this.twitter = {
+                active: undefined !== props.twitter.active ? props.twitter.name : false,
+                name: undefined !== props.twitter.name ? props.twitter.name : null,
+                screen_name: undefined !== props.twitter.screen_name ? props.twitter.screen_name : null,
+                description: undefined !== props.twitter.description ? props.twitter.description : null,
+            }
         }
     }
 }
