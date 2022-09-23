e7dacb02766f1f4d9cb9dc7bfdfefb0a19f81152
batabana <36864084+batabana@users.noreply.github.com>
2022-09-23 16:57:49 +0200

    :children_crossing: Set map view to defined point(s)

--- src/components/message/MessageMap.tsx
+++ src/components/message/MessageMap.tsx
@@ -1,4 +1,4 @@
-import { latLng } from 'leaflet'
+import { latLng, LeafletEvent } from 'leaflet'
 import React, { FC } from 'react'
 import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet'
 import { Message } from '../../api/Message'
@@ -26,10 +26,33 @@ const MessageMap: FC<Props> = ({ message, ticker }) => {
     return null
   }
 
+  const handleDataAdd = (event: LeafletEvent) => {
+    const leafletLayer = event.target
+    const features = Object.values(leafletLayer._layers)
+
+    if (
+      features.length === 1 &&
+      // type is currently not defined
+      // @ts-ignore
+      features[0].feature.geometry.type === 'Point'
+    ) {
+      // @ts-ignore
+      const coords = features[0].feature.geometry.coordinates
+      leafletLayer._map.setView([coords[1], coords[0]], 13)
+    } else {
+      leafletLayer._map.fitBounds(leafletLayer.getBounds())
+    }
+  }
+
   return (
     <MapContainer center={position} scrollWheelZoom={false} zoom={7}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <GeoJSON data={JSON.parse(message.geo_information)} />
+      <GeoJSON
+        data={JSON.parse(message.geo_information)}
+        eventHandlers={{
+          add: handleDataAdd,
+        }}
+      />
     </MapContainer>
   )
 }
