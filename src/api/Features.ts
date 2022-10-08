dfbc903d1c6abf96d74720e63058bd396f2d6ab2
louis <louis@systemli.org>
2022-10-08 22:06:39 +0200

    ✨ Add Feature Detection for Twitter/Telegram

---
+++ src/api/Features.ts
@@ -0,0 +1,26 @@
+import { ApiUrl, Response } from './Api'
+
+interface FeaturesResponseData {
+  features: Features
+}
+
+export interface Features {
+  twitter_enabled: boolean
+  telegram_enabled: boolean
+}
+
+export function useFeatureApi(token: string) {
+  const headers = {
+    Accept: 'application/json',
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  }
+
+  const getFeatures = (): Promise<Response<FeaturesResponseData>> => {
+    return fetch(`${ApiUrl}/admin/features`, {
+      headers: headers,
+    }).then(response => response.json())
+  }
+
+  return { getFeatures }
+}
