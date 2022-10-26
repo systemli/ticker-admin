cdd91d5a705e696919030b470d853f9978a07edc
louis <louis@systemli.org>
2022-10-26 22:51:56 +0200

    💄 Add Loader Component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,15 @@
+import { CircularProgress, Stack, Typography } from '@mui/material'
+import React, { FC } from 'react'
+
+const Loader: FC = () => {
+  return (
+    <Stack alignItems="center" justifyContent="center" sx={{ m: 10 }}>
+      <CircularProgress size="3rem" />
+      <Typography component="span" sx={{ pt: 2 }} variant="h5">
+        Loading
+      </Typography>
+    </Stack>
+  )
+}
+
+export default Loader
