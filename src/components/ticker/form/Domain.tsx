335ee3a71681bda97c50fc697f39a817d4975c0d
louis <louis@systemli.org>
2024-04-28 14:25:56 +0200

    🚸 Disallow users to change the ticker domain

--- src/components/ticker/form/Domain.tsx
+++ src/components/ticker/form/Domain.tsx
@@ -1,11 +1,19 @@
 import { TextField } from '@mui/material'
 import { FC } from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
+import useAuth from '../../useAuth'
 
 const Domain: FC = () => {
   const { control } = useFormContext()
+  const { user } = useAuth()
 
-  return <Controller name="domain" control={control} render={({ field }) => <TextField {...field} label="Domain" margin="dense" required />} />
+  return (
+    <Controller
+      name="domain"
+      control={control}
+      render={({ field }) => <TextField {...field} label="Domain" margin="dense" required disabled={!user?.roles.includes('admin')} />}
+    />
+  )
 }
 
 export default Domain
