a145dc88978e45ae9d7fb150d2e649cf71368224
doobry <doobry@systemli.org>
2024-06-25 18:15:49 +0200

    Add icons to ticker config modal tabs

--- src/components/ticker/TickerModalForm.tsx
+++ src/components/ticker/TickerModalForm.tsx
@@ -1,4 +1,6 @@
 import { Tab, Tabs } from '@mui/material'
+import Tune from '@mui/icons-material/Tune'
+import Campaign from '@mui/icons-material/Campaign'
 import React, { FC, useState } from 'react'
 import { Ticker } from '../../api/Ticker'
 import Modal from '../common/Modal'
@@ -22,8 +24,8 @@ const TickerModalForm: FC<Props> = ({ onClose, open, ticker }) => {
   return (
     <Modal fullWidth onClose={onClose} open={open} submitForm={tabValue === 0 ? 'tickerForm' : undefined} title={ticker ? 'Configure Ticker' : 'Create Ticker'}>
       <Tabs onChange={handleTabChange} value={tabValue}>
-        <Tab label="General" />
-        <Tab disabled={!ticker} label="Social Connections" />
+        <Tab icon={<Tune />} iconPosition="start" label="General" />
+        <Tab disabled={!ticker} icon={<Campaign />} iconPosition="start" label="Social Connections" />
       </Tabs>
       <TabPanel index={0} value={tabValue}>
         <TickerForm callback={onClose} id="tickerForm" ticker={ticker} />
