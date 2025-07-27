ec80dc9a97ada9ee0bb5b3c10c5a8bbdd8defe27
louis <louis@systemli.org>
2025-07-27 17:23:51 +0200

    🧑‍💻 Use npm and nvm for Node and Package Management

--- src/hooks/useDebounce.test.tsx
+++ src/hooks/useDebounce.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
-import { renderHook, act } from '@testing-library/react-hooks'
+import { act, renderHook } from '@testing-library/react'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import useDebounce from './useDebounce'
 
 describe('useDebounce', () => {
